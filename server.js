b76cf66f8939c4c80aaac77a8e7d8de9fa819536
German Gamboa <germangamboa95@gmail.com>
2018-06-20 11:07:52 -0400

    allow cors for testing

--- server.js
+++ server.js
@@ -19,16 +19,11 @@ const sessions = require("./middleware/sessions.js");
 const locations = require('./middleware/locations.js');
 
 const images = require('./middleware/images.js');
-
+const cors = require('cors');
 // middleware goes here
+app.use(cors())
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  next();
-});
 
 app.get('/', function(req, res,){
   res.send('Use the api endpoints!')
